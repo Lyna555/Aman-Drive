f2c23fd8db61c33a8a41d71f78e595fc8167da00
Lyna555 <bouglina3@gmail.com>
2025-08-08 21:19:44 +0100

    change db pool

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
-    'pool_size': 2,
-    'max_overflow': 0,
+    'pool_size': 10,
+    'max_overflow': 20,
     'pool_timeout': 30,
     'pool_recycle': 1800
 }
