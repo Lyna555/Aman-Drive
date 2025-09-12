76580f8f8460a0d3e5f8e2695648519dec3d40b5
Lyna555 <bouglina3@gmail.com>
2025-09-12 15:14:12 +0100

    fix update client

--- controllers/client_controller.py
+++ controllers/client_controller.py
@@ -114,8 +114,9 @@ def create_client(current_user):
 # Update a client
 @token_required
 @role_required('client', 'insurance', 'admin')
-def update_client(client_id):
-    client = Client.query.get(client_id)
+def update_client(current_user):
+    client = Client.query.filter_by(user_id=current_user.id).first()
+    
     if not client:
         return jsonify({'error': 'Client not found'}), 404
 
